23b442272d4fc1caf160c88c816ccd9ac6459b2f
MichaelSeebregts <mjseebregts@gmail.com>
2025-05-02 16:51:51 +0200

    update

--- Tut2.R
+++ Tut2.R
@@ -6,3 +6,38 @@ names(eg)
 truef = eg$truth 
 persp(truef$x, truef$z, truef$f, col = heat.colors(40), main = "a. truth")
 
+rep((1:8-0.5)/8, rep(8,8))
+
+
+b1 = gam(y ~ te(x, z, k = 10), data = eg$data)
+b1
+
+vis.gam(b1, theta = 30, phi = 30)
+
+ind = sample(1:n, 20, replace = FALSE)
+b2 = gam(y ~ te(x, z, k = 20), data = eg$data, knots = list(x = eg$data$x[ind], z = eg$data$z[ind]))
+b2
+
+vis.gam(b2, theta = 30, phi = 30)
+  
+b3 = gam(y ~ te(x, z, fx = TRUE), data = eg$data)
+b3
+
+vis.gam(b3, theta = 30, phi = 30)
+
+
+b4 = gam(y ~ tp(x, z, k = 10), data = eg$data)
+b4
+
+vis.gam(b4, theta = 30, phi = 30)
+
+ind = sample(1:n, 20, replace = FALSE)
+b5 = gam(y ~ te(x, z, k = 20), data = eg$data, knots = list(x = eg$data$x[ind], z = eg$data$z[ind]))
+b5
+
+vis.gam(b5, theta = 30, phi = 30)
+
+b6 = gam(y ~ te(x, z, fx = TRUE), data = eg$data)
+b6
+
+vis.gam(b6, theta = 30, phi = 30)
